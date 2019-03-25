a57c7150e9d5e969ad887eb33fca95afffd97b03
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-25 15:01:24 +0100

    Création des constructeurs de boutons (ActionListener à ajouter)

---
+++ Jeux_enfant/src/Marianne/Bouton.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Marianne;
+
+import java.awt.Font;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Yann Golhen
+ */
+public class Bouton extends JButton{
+
+    public Bouton(String name, Font font){
+        
+        super(name);
+        this.setFont(font);
+    }
+
+    public Bouton(String name){
+        
+        super(name);
+    }
+    
+}
