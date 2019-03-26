eaca21b3bc096a6937738ab51e60ce7937833e30
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-26 10:52:57 +0100

    Premier commit de Martinez

--- Jeux_enfant/src/zine/Testingstuff.java
+++ Jeux_enfant/src/zine/Testingstuff.java
@@ -6,6 +6,7 @@
 
 package zine;
 
+import Pkg_DAO.Question_Julien;
 import javax.swing.JFrame;
 
     
@@ -15,6 +16,7 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.Random;
  
 public class Testingstuff extends JFrame{
     private JLabel theBox;
@@ -22,11 +24,15 @@ public class Testingstuff extends JFrame{
     private String str;
      private JPanel pan = new JPanel();
     private JButton bouton = new JButton("question suivante");
+    String text;
+    
     public Testingstuff(){
         super("question");
+        text = textQuestion();
+        
         setLayout(new FlowLayout());  
         JPanel box= new JPanel();
-        theBox = new JLabel();
+        theBox = new JLabel(text);
         theBox2 = new JTextField(20);
         pan.add(bouton);
         this.setContentPane(pan);
@@ -38,25 +44,74 @@ public class Testingstuff extends JFrame{
     }
      
     private class HandlerClass implements ActionListener{
+        @Override
         public void actionPerformed(ActionEvent e){
             str = e.getActionCommand(); //This is what I mean by storing.
             // now I want 3 variables, collecting data from 3 textbox
             // at the same time. Please help?
              
             // This is how I can print out in GUI:
-            JOptionPane.showMessageDialog(null, str);
+            //JOptionPane.showMessageDialog(null, str);
+            
+        String text = "";
+        Question_Julien daop = new Question_Julien();
+        Question a = daop.find(1);
+        text = a.getReponse();
+        
+        if(str.equals(text))
+        {
+            JOptionPane.showMessageDialog(null
+                    ,"C'est juste bien joué"
+                    ,"Bravo!"
+                    ,JOptionPane.ERROR_MESSAGE);
+        }
+        
+        else
+        {
+            JOptionPane.showMessageDialog(null
+                    ,"C'est faux"
+                    ,"BOUH"
+                    ,JOptionPane.ERROR_MESSAGE);
+        }
+            
         }   }   
   
 
-    private static 
+    private static String textQuestion()
+    {
+        String text = "";
+        Question_Julien daop = new Question_Julien();
+        Question a = daop.find(1);
+        text = a.getQuestions();
+        return text;
+    }
+    private static int ChoixQuestion()
+    {
+        int nbr;
+        int choixNiveau = 1;
+        
+        Random r = new Random();
+        int nb = r.nextInt((9 - 0) + 1) + 0;
+        Question_Julien daop = new Question_Julien();
+        Question a = daop.find(nb);
+        nbr = a.getNiveau_question();
+        if(nbr != choixNiveau)
+        {
+            return ChoixQuestion();
+        }
+        
+        
+        return nbr;
+    }
 
-    public static void main(String[]args){      
+public static void main(String[]args){      
         Testingstuff t = new Testingstuff();
         t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         t.setSize(350,100);
         t.setVisible(true);
        
-    }   }
+    }   
+}
 
 
 
