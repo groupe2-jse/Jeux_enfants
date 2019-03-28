4242d928947d5b048eaee8e57032eb94e74cd349
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-28 15:36:18 +0100

    Ajout image pendu

--- Jeux_enfant/src/Marianne/Display_drawing.java
+++ Jeux_enfant/src/Marianne/Display_drawing.java
@@ -12,7 +12,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import javax.swing.JPanel;
-import javax.xml.bind.annotation.XmlElement;
 
 /**
  *
